df2ff30c49b409e16c2725f0bf4c6f0e62e7c2d8
Joshua Thomas <josh.thomas7@yahoo.com>
2017-06-10 18:19:14 -0400

    Added decoration, dealer, and winner functunality

--- Game_Project/js/app.js
+++ Game_Project/js/app.js
@@ -1,11 +1,31 @@
-var mainDeck = new CardDeck ();
-var me = new Player();
+var theGame = new NewGame();
 
 $(document).ready (function(){
-  me.initialDeal();
+
+  theGame.start();
 
   $('#hit-btn').click(function (){
-    me.hit();
+    theGame.playerHit();
+    theGame.player.checkStatus();
+    theGame.player.showHand();
+    setTimeout (function (){
+      if (theGame.player.totalPoints > 21 ){
+        alert('You lose');
+        theGame.reset ();
+        theGame.start ();
+      }
+    }, 500);
+
   });
+
+  $('#stand-btn').click(function (){
+    theGame.dealer.playTurn();
+    theGame.player.checkStatus();
+    alert(theGame.checkWinner()+' DealerScore: ' + theGame.dealer.totalPoints);
+    theGame.reset ();
+    theGame.start ();
+  });
+  $('.dealerValue').html(theGame.dealer.totalPoints);
+
 
 });
--- Game_Project/js/black-jack.js
+++ Game_Project/js/black-jack.js
@@ -1,3 +1,8 @@
+function NewGame () {
+  this.deck = new CardDeck();
+  this.player = new Person ();
+  this.dealer = new Dealer ();
+}
 
 // CONSTRUCTOR FUNCTION FOR EACH SUIT
 function SuitObject (suitName) {
@@ -49,31 +54,111 @@ CardDeck.prototype.randomCard = function() {
   return new SingleCard(suitValue, randomCardValue, checkPointValue(randomCardValue));
 };
 //KEEPS TRACK OF THE PLAYERS POINTS AND CARDS IN HAND
+
 function Player () {
   this.totalPoints = 0;
   this.hand = [];
+  this.status = 'Hit or Stand?';
+  this.game = theGame;
 }
-// DEALS OUT TWO CARDS INTO PLAYERS HANDS AND DISPLAYS IT
-Player.prototype.initialDeal = function () {
-  for (var i = 0; i < 2; i++) {
-    var someCard = mainDeck.randomCard();
-    this.totalPoints += someCard.pointValue;
 
-    $('.displayCards').append(
-    '<span><h5> Card Suit: </h5>' + someCard.cardSuit +
-    '</span><span><h5>Face Value: </h5>' + someCard.faceValue +
-    '</span><span><h5>Point Value: </h5>' +someCard.pointValue + '</span><br>');
+// PERSON IS THE PERSON PLAYING THE GAME
+function Person () {
+  Player.call(this);
+ }
 
-    $('.totalValue').html('<h3>Total Value: ' + this.totalPoints + '</h3>');
+//INHERITS FROM PLAYER
+Person.prototype = Object.create(Player.prototype);
 
-    this.hand.push(someCard);
+function Dealer () {
+  Player.call(this);
+}
+
+Dealer.prototype = Object.create(Player.prototype);
+
+Dealer.prototype.playTurn = function() {
+  while (this.totalPoints < 17) {
+    theGame.dealerHit();
   }
 };
 
-//Adds a card to the players hands 
+// DEALS OUT TWO CARDS INTO PLAYERS HANDS AND DISPLAYS IT
+NewGame.prototype.initialDeal = function () {
+  for (var i = 0; i < 2; i++) {
+    var someCard = this.deck.randomCard();
+    var someCard3 = this.deck.randomCard();
+    this.player.totalPoints += someCard.pointValue;
+      this.player.hand.push(someCard);
+      this.dealer.totalPoints += someCard3.pointValue;
+        this.dealer.hand.push(someCard3);
+  }
+};
 
-Player.prototype.hit = function () {
-    var someCard2 = mainDeck.randomCard();
-    this.totalPoints += someCard2.pointValue;
-    this.hand.push(someCard2);
+//Adds a card to the players hands
+
+NewGame.prototype.playerHit = function () {
+    var someCard2 = this.deck.randomCard();
+    this.player.totalPoints += someCard2.pointValue;
+    this.player.hand.push(someCard2);
+};
+
+NewGame.prototype.dealerHit = function () {
+  var someCard2 = this.deck.randomCard();
+  this.dealer.totalPoints += someCard2.pointValue;
+  this.dealer.hand.push(someCard2);
+};
+//Makes message for player
+Player.prototype.checkStatus = function(){
+  if (this.totalPoints > 21) {
+    this.status = 'Busted';
+  }
+  else if (this.totalPoints === 21) {
+    this.status = 'Twenty-one!';
+  }
+  else {
+    this.status = 'Hit or Stand?';
+  }
+};
+
+var cardPosition = 0;
+// Displays INFO
+Person.prototype.showHand = function () {
+  $('.aCard').html('');
+  $('.aCard').hide();
+  this.hand.forEach(function (card) {
+    cardPosition += 1;
+      $('.card-'+cardPosition).show();
+      $('.card-'+cardPosition).append(
+      '<span><h5> Card Suit: </h5>' + card.cardSuit +
+      '</span><span><h5>Face Value: </h5>' + card.faceValue +
+      '</span><span><h5>Point Value: </h5>' + card.pointValue + '</span><br>');
+    });
+    $('.totalValue').html('<h3>Total Value: ' + this.totalPoints + '</h3>');
+    $('.status').html(this.status);
+    cardPosition = 0;
+};
+
+
+// Checks is the Dealer has a higher score than the player
+NewGame.prototype.checkWinner = function () {
+  if (this.player.totalPoints >= this.dealer.totalPoints || this.dealer.totalPoints >= 21 ) {
+    return 'You win!';
+  }
+  else {
+    return 'You lose...';
+  }
+};
+
+// Starts New Game
+NewGame.prototype.start = function() {
+  this.initialDeal();
+  this.player.checkStatus();
+  this.player.showHand();
+};
+
+// Resets Game
+NewGame.prototype.reset = function (){
+  this.player = new Person ();
+  this.dealer = new Dealer ();
+  this.deck = new CardDeck ();
 };
